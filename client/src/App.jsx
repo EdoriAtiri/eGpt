13276109f29873ee814f1edd933e2bf1376fc932
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-11-21 15:53:52 +0100

    Add chatMessage box for user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,14 @@ function App() {
         </aside>
 
         <section className='chatbox'>
+          <div className='chat-log'>
+            <div className='chat-message'>
+              <div className='chat-message-center'>
+                <div className='avatar'></div>
+                <div className='message'>Hello World</div>
+              </div>
+            </div>
+          </div>
           <div className='chat-input-holder'>
             <textarea rows='1' className='chat-input-textarea'></textarea>
           </div>
