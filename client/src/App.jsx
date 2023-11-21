61544c8677989f312d7c5f4dcc7b4d932bd712c2
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-11-21 15:22:37 +0100

    Create initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+import './normal.css'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <aside className='sidemenu'>
+          <div className='side-menu-button'>
+            <span>+</span> New Chat
+          </div>
+        </aside>
+
+        <section className='chatbox'></section>
+      </div>
+    </>
+  )
+}
+
+export default App
