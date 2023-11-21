a4920af33e27e7fc0e2986a3defaffa57585df15
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-11-21 15:59:06 +0100

    Add chatMessage box for ai

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,13 @@ function App() {
                 <div className='message'>Hello World</div>
               </div>
             </div>
+
+            <div className='chat-message chatgpt'>
+              <div className='chat-message-center'>
+                <div className='avatar chatgpt'></div>
+                <div className='message'>I am an AI</div>
+              </div>
+            </div>
           </div>
           <div className='chat-input-holder'>
             <textarea rows='1' className='chat-input-textarea'></textarea>
